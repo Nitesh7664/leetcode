a4ea6b8964721ed463c31ea9df1816f02819a73f
Nitesh Kumar Sachidanand Sharma <57126225+Nitesh7664@users.noreply.github.com>
2025-07-20 16:47:46 +0530

    Time: 2 ms (96.72%) | Memory: 42.1 MB (19.29%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -4,16 +4,13 @@ class Solution {
         for (char ch: str.toCharArray()) {
             if (ch == '(' || ch == '{' || ch == '[')  {
                 s.push(ch);
-                System.out.println(ch + " if");
             } else {
                 if (s.isEmpty()) return false;
                 char stackTop = s.pop();
-                System.out.println(ch + " " + stackTop + " if");
                 if (stackTop == '(' && ch != ')' || stackTop == '{' && ch != '}' || stackTop == '[' && ch != ']') return false;
 
             }
         }
-        System.out.println("stack empty check " + s.isEmpty());
         return s.isEmpty();
     }
 }
